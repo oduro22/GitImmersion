63bcb1d6a477c1aa77284ea15a19bddbef70d14a
Oresti Duro <oduro@worcester.edu>
2019-02-22 14:00:46 -0500

    first commit

--- hello.rb
+++ hello.rb
@@ -1,7 +1,7 @@
 require 'greeter'
 
-puts "What's your name"
-my_name = gets.strip
+# Default is World
+name = ARGV.first || "World"
 
-greeter = Greeter.new(my_name)
+greeter = Greeter.new(name)
 puts greeter.greet
